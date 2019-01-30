1013759957006f16d545be8391ea7abb635345a9
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-30 06:58:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,29 @@
+ require 'pry'
+def my_collect(languages)
+  i = 0 
+  collection = []
+  while i > languages.length 
+    collecton << yield(languages.upcase)
+    i += 1
+    binding.pry 
+  end
+  collection 
+end  
 
-
+=begin def empty_array = []
+    counter = 0
+    
+      my_collect(empty_array) do |x|
+        counter += 1
+      end
+end 
+=begin def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection <<
+    yield(array[i])
+    i += 1
+  end
+  collection 
+end  
